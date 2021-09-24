0e55fbca89cc9188b7995e0829639e2d8e10f88b
mrbas3 <valkyriesv9@gmail.com>
2021-09-24 14:02:03 +0700

    Update webhook_api.php

--- webhook_api.php
+++ webhook_api.php
@@ -27,7 +27,9 @@ foreach ($request_json['events'] as $event)
             }
             if($text == "พิกัดอัตราภาษีสรรพสามิต"){
                 $reply_message = mySQL_selectAll('http://bot.kantit.com/json_select_users.php');
-            }else if($text == "อัตราภาษีสรรพสามิตรถยนต์ำ"){
+            }else if($text == "ขายสุรายาสูบและไพ่"){
+                $reply_message = mySQL_selectAll('http://bot.kantit.com/json_select_users.php?sid='.$texts[1]);
+            }else if($text == "อัตราภาษีสรรพสามิตรถยนต์"){
                 $reply_message = mySQL_selectAll('http://bot.kantit.com/json_select_users.php?sid='.$texts[1]);
             }else if($text == "ขั้นตอนการขอใบอนุญาตขายสุรา"){
                     $reply_message = mySQL_selectFTP('http://bot.kantit.com/json_select_ftp.php?sid='.$texts[1]);                    
